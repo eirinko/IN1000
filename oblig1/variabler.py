958b9e1919785bbd3659da7e595483e6ace78f68
Eirin Korvald <eirinkorvald.ek@gmail.com>
2017-08-25 20:36:12 +0200

    Ferdig med variabler.py.

---
+++ oblig1/variabler.py
@@ -0,0 +1,21 @@
+#Oppgave 2.1
+x=2
+y=3
+print(x)
+print(y)
+
+# Oppgave 2.2
+sum=x+y
+print("Sum: {}".format(sum))
+
+#Oppgave 2.3
+a="Hello"
+b="world"
+print(a)
+print(b)
+sammen=a+b
+print(sammen)
+
+#Oppgave 2.4
+sammen="{} {}".format(a,b)
+print(sammen)

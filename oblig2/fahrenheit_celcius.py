b1e17acd0b6313fe36874ec43c68f3a83b6c88c2
Eirins laptop <eirinkorvald.ek@gmail.com>
2017-08-28 21:16:36 +0200

    Ferdig med fahrenheit_celcius.py

---
+++ oblig2/fahrenheit_celcius.py
@@ -0,0 +1,17 @@
+#Oppg. 2.1
+Temp_F=70
+
+#Oppg.2.2.
+print(Temp_F)
+
+#Oppg. 2.3
+Temp_C=(Temp_F-32)*5/9
+
+#Oppg. 2.4
+print(Temp_C)
+
+#oppg. 2.5 Her er det ikke tatt høyde for at folk kan skrive andre ting enn tall.
+Temp_F=int(input("Hva er temperaturen i Fahrenheit? "))
+print(Temp_F)
+Temp_C=(Temp_F-32)*5/9
+print(Temp_C)

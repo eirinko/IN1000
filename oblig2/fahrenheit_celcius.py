7c03ad01b6af3613c7354dd807deecfba6c9c4ac
Eirins laptop <eirinkorvald.ek@gmail.com>
2017-08-29 10:55:08 +0200

    fahrenheit_celcius.py er ferdig.

--- oblig2/fahrenheit_celcius.py
+++ oblig2/fahrenheit_celcius.py
@@ -1,13 +1,13 @@
-#Oppg. 2.1
+#Oppg. 2.1 Temp_F er temperaturen i Fahrenheit.
 Temp_F=70
 
-#Oppg.2.2.
+#Oppg.2.2. Skriver ut temperaturen i Fahrenheit.
 print(Temp_F)
 
-#Oppg. 2.3
+#Oppg. 2.3 Regner ut temperatur i Celcius.
 Temp_C=(Temp_F-32)*5/9
 
-#Oppg. 2.4
+#Oppg. 2.4 Skriver ut temperturen i Celcius.
 print(Temp_C)
 
 #oppg. 2.5 Her er det ikke tatt høyde for at folk kan skrive andre ting enn tall.
@@ -15,3 +15,6 @@ Temp_F=int(input("Hva er temperaturen i Fahrenheit? "))
 print(Temp_F)
 Temp_C=(Temp_F-32)*5/9
 print(Temp_C)
+
+#For å ta høyde for at folk kanskje skriver andre ting enn tall, kan vi f.eks. ha en if-setning som skriver ut en feilmelding hvis brukeren skriver noe annet.
+

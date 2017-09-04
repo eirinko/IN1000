6dedee0346baa94762fcb5f7c27cff7e07bd58bd
Eirins laptop <eirinkorvald.ek@gmail.com>
2017-09-04 21:16:19 +0200

    Fullført sirkel.py

---
+++ oblig3/sirkel.py
@@ -0,0 +1,21 @@
+#Oppg.3.1-4. Bruke modulen ezGraphics.py for å tegne figurer.
+#Henter først ut GraphicsWindow fra exgraphics, der figurene kan tegnes.
+
+from ezgraphics import GraphicsWindow
+
+# Lage et vindu for å tegne i, 500x500 pixels.
+vindu = GraphicsWindow(500, 500)
+
+# Setter tittel på vinduet.
+vindu.setTitle("Oppg. 3: Rød sirkel")
+
+#Gir navn til vinduet for å bruke videre i programmeringen.
+canvas = vindu.canvas()
+
+#Gir sirkelen rødfarge.
+canvas.setOutline("red")
+
+# canvas.setFill("red") kan brukes for å lage det japanske flagget.
+
+#canvas.drawOval(x, y, width, height) brukes for å tegne sirkel.
+canvas.drawOval(100,100,300,300)

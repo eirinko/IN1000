d33da91fc3285983c5b7ea9e676da8807f2d4a0b
eirinko <eirinkorvald.ek@gmail.com>
2017-10-04 13:42:17 +0200

    Finished with teori.py.

---
+++ oblig6/teori.py
@@ -0,0 +1,19 @@
+#Oppg. 3.1. Innkapsling innebaerer at vi samler data og kode i objekter,
+#der maaten objektet har representert og manipulerer dataene er skjult.
+#Alle endringer som gjoeres paa instansvariablene for klassen gjoeres av klassens metoder.
+
+#Vi trenger altsaa ikke vite hva som skjer og kan forholde oss bare til input-output.
+#Fordeler: Vi kan bruke metodene gang paa gang uten aa skrive dem paa nytt.
+#Det er lettere aa ha oversikt over store mengder data og aa loese komplekse problemer.
+
+#Oppg.3.2. Grensesnittet til en klasse:
+#Det er det vi ser uten aa gaa inn i koden til klassen.
+#Implementasjonen av en klasse er der vi definerer hva som skal vaere med,
+#hvilke metoder som skal vaere tilgjengelig og hvilke variable klassen har.
+#Grensesnittet er der vi kaller paa metodene. Vi ber om noe og faar noe ut,
+#som en svart boks av operasjoner der metodene er den svarte boksen.
+
+#Oppg.3.3. 
+#En instansmetode er koblet til kun en klasse og kan bare gjoeres med denne klassen.
+#Prosedyrer og funksjoner er mulig aa bruke paa alt.
+

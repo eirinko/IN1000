2e7771c9b2b49370ea017f0298edda24e76e8bb4
Eirin Korvald <eirinko@gilly.ifi.uio.no>
2017-10-18 13:40:12 +0200

    Added feature of checking whether the inputs for row and column are positive. If values of 0 or <0 is used, the user will be asked to do it again.

--- oblig7/main.py
+++ oblig7/main.py
@@ -3,15 +3,17 @@ from spillebrett import Spillebrett
 def main():
     rader=int(input("Hvor mange rader vil du at spillebrettet skal ha? \n"))
     kolonner=int(input("Hvor mange kolonner vil du at spillebrettet skal ha? \n"))
-    brett1=Spillebrett(rader,kolonner)
 
-    while True:
-        brett1.tegnBrett()
-        brett1.oppdatering()
-
-        print("Generasjon: {} - Antall levende celler: {}".format(brett1.generasjon(),brett1.finnAntallLevende()))
-        svar=input("Press enter for aa fortsette eller q og enter for aa avslutte: ")
-        if svar=="q":
-            break
+    if (rader>0) and (kolonner>0):
+        brett1=Spillebrett(rader,kolonner)
+        while True:
+            brett1.tegnBrett()
+            brett1.oppdatering()
+            print("Generasjon: {} - Antall levende celler: {}".format(brett1.generasjon(),brett1.finnAntallLevende()))
+            svar=input("Press enter for aa fortsette eller q og enter for aa avslutte: ")
+            if svar=="q":
+                break
+    else:
+        print("Programmet kjoerer bare med positive verdier over 0. \nProev programmet igjen med positive verdier fra 1 og oppover.")
 
 main()
